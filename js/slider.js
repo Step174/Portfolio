44ce593718c85213c1b431b969f6b5e75994e198
Step174 <itstep9328@gmail.com>
2017-04-11 22:46:29 +0500

    плавный скрол

--- js/slider.js
+++ js/slider.js
@@ -14,3 +14,25 @@ arrowleft.addEventListener("click", function() {
     content1.classList.add("show-prew");
     content2.classList.remove("show-prew");
 });
+
+document.querySelectorAll('[href^="#new"]').forEach(function(lnk) {
+  var speed = 0.3;
+  lnk.onclick = function() {
+    var w = window.pageYOffset,
+      hash = this.href.replace(/[^#]*(.*)/, '$1');
+    t = document.querySelector(hash).getBoundingClientRect().top
+    start = null;
+    requestAnimationFrame(step);
+    function step(time) {
+      if (start === null) start = time;
+      var progress = time - start,
+        r = (t < 0 ? Math.max(w - progress / speed, w + t) : Math.min(w + progress / speed, w + t));
+      window.scrollTo(0, r);
+      if (r != w + t) {
+        requestAnimationFrame(step)
+      } else {
+        location.hash = hash
+      }
+    }
+  }
+});
