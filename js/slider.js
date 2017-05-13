cbcaeb7c4668a3213d63343630c6a7d0ef7a4c61
Step174 <itstep9328@gmail.com>
2017-05-14 01:41:56 +0500

    паро

--- js/slider.js
+++ js/slider.js
@@ -37,3 +37,50 @@ document.querySelectorAll('[href^="#new"]').forEach(function(lnk) {
         }
     }
 });
+
+
+
+
+
+
+document.getElementById('feedback-form').addEventListener('submit', function(evt){
+  var http = new XMLHttpRequest(), f = this;
+  evt.preventDefault();
+  http.open("POST", "contacts.php", true);
+  http.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+  http.send("nameFF=" + f.nameFF.value + "&contactFF=" + f.contactFF.value + "&messageFF=" + f.messageFF.value);
+  http.onreadystatechange = function() {
+    if (http.readyState == 4 && http.status == 200) {
+      alert(http.responseText + ', Ваше сообщение получено.\nНаши специалисты ответят Вам в течении 2-х дней.\nБлагодарим за интерес к нашей фирме!');    
+      f.messageFF.removeAttribute('value'); // очистить поле сообщения (две строки)
+      f.messageFF.value='';
+    }
+  }
+  http.onerror = function() {
+    alert('Извините, данные не были переданы');
+  }
+}, false);
+
+
+var active = document.querySelector(".btn-work");
+var popup = document.querySelector(".forma");
+var closePopup = document.querySelector(".close");
+
+active.addEventListener("click", function(event) {
+    event.preventDefault();
+    popup.classList.toggle("popup-show");
+})
+
+closePopup.addEventListener("click", function(event) {
+    event.preventDefault();
+    popup.classList.remove("popup-show");
+
+});
+
+window.addEventListener("keydown", function(event) {
+    if (event.keyCode === 27) {
+        if (popup.classList.contains("popup-show")) {
+            popup.classList.remove("popup-show");
+        }
+    }
+});
